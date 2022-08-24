be589bc74dceb8e8477049089d41309b647dfe8c
kadiraswapna123 <kadiraswapna@gmail.com>
2022-08-24 18:48:40 +0530

    CabInvoiceGenerator

---
+++ RideType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenaretor
+{
+    internal class RideType
+    {
+        public enum RideType { NORMAL, PREMIUM}
+    }
+}
